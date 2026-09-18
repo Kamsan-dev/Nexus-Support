81ec89fb44ed0727a3657f0f6cfdb539afbb6cf4
Kamsan-dev <rim3968@hotmail.fr>
2026-09-18 23:43:24 +0200

    feat(ticket-service): Add endpoints for ticket resource. WIP.

---
+++ ticketservice/src/main/java/com/kamsan/userservice/dto/TicketDetailsDTO.java
@@ -0,0 +1,29 @@
+package com.kamsan.userservice.dto;
+
+import com.kamsan.userservice.enumeration.TicketPriority;
+import com.kamsan.userservice.enumeration.TicketStatus;
+import com.kamsan.userservice.enumeration.TicketType;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * DTO for {@link com.kamsan.userservice.model.Ticket}
+ */
+public record TicketDetailsDTO(OffsetDateTime createdAt,
+                               OffsetDateTime updatedAt,
+                               @NotNull UUID ticketPublicId,
+                               @NotNull UUID issuerPublicId,
+                               UUID assigneePublicId,
+                               String title,
+                               String description,
+                               @Min(0) @Max(100) int progress,
+                               TicketStatus status,
+                               TicketPriority priority,
+                               TicketType type,
+                               OffsetDateTime dueDate) implements Serializable {
+}
